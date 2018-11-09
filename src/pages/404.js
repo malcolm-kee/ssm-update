96a415714c8b3aceac525dfb213725e10d332a70
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-11-09 17:42:09 +0800

    add link in 404 to go home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import Layout from '../components/layout'
+import { Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/layout';
 
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>The page you're looking for doesn&#39;t exist... the sadness.</p>
+    <Link to="/" className="button">
+      Back to home
+    </Link>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
